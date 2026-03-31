bdf314b027e5fafcd15d0641de57d6d72c52cd90
Adrian-dre <adriancaberoy1@gmail.com>
2026-03-31 20:23:28 +0800

    added clk_div

--- seq_101.v
+++ seq_101.v
@@ -23,6 +23,8 @@ module seq_101(out,state,in,clk,rst_n);
  input            rst_n;
  output reg       out;
  output reg [1:0] state;
+ 
+ // Clock 3s period 
 
  // state assignment
  parameter [1:0] S0= 2'b00;
